9c3bb0725d1479a0b7c6e6109d1e8a8cf7e361aa
Ham I-sik(a.k.a 2sik, bis) <ham2sik@gmail.com>
2020-07-30 10:15:04 +0900

    Update quiz_1.js

--- programmers/Week 5/quiz_1.js
+++ programmers/Week 5/quiz_1.js
@@ -3,9 +3,9 @@ function solution(s) {
         strLength = s.length,
         quot = parseInt(strLength / 2);
     if (strLength % 2) {
-        answer = s.substr(quot, 1);
+        answer = s.substring(quot, quot + 1);
     } else {
-        answer = s.substr(quot - 1, 2 );
+        answer = s.substring(quot - 1, quot + 1 );
     }
     return answer;
 }
