b7ce019e296907bcb53218e2ec748fb464c48571
Ham I-sik(a.k.a 2sik, bis) <ham2sik@gmail.com>
2020-08-06 09:09:07 +0900

    Create quiz_2.js

---
+++ programmers/Week 5/quiz_2.js
@@ -0,0 +1,13 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12922
+
+function solution(n) {
+    var answer = '';
+    for (let i = 1; i <= n ; i++ ) {
+        if (i % 2 == 1 ) {
+            answer += "수"
+        } else {
+            answer += "박"
+        }
+    }
+    return answer;
+}
