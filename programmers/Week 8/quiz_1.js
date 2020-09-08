e6d709329475479a896d244ca381882b82c61912
Ham I-sik(a.k.a 2sik, bis) <ham2sik@gmail.com>
2020-09-08 09:22:59 +0900

    Update quiz_1.js

--- programmers/Week 8/quiz_1.js
+++ programmers/Week 8/quiz_1.js
@@ -2,35 +2,24 @@
 
 function solution(people, limit) {
     var answer = 0;
-    people.sort((a, b) => a - b);
+    people.sort((a, b) => b - a);
     // console.log(people);
+    var minWeightNum  = people.length - 1;
     
-    function count(peopleArray) {
-        if (limit - peopleArray[0] >= peopleArray[0]) {
-            var peopletLength = peopleArray.length;
-            for (let i = 0; i < peopletLength; i ++) {
-                // console.log(i);
-                var arrayNum = peopletLength - 1 - i;
-                if (peopleArray[0] + peopleArray[arrayNum] > limit) {
-                    answer ++;
-                    people.splice(arrayNum, 1);
-                } else {
-                    answer ++;
-                    people.splice(arrayNum, 1);
-                    people.splice(0, 1);
-                    count(people);
-                    break;
-                }
-            } 
+    for (let i = 0; i < people.length; i ++) {
+        if (i < minWeightNum) {
+            if (people[i] + people[minWeightNum] > limit) {
+                answer ++;
+            } else {
+                answer ++;
+                minWeightNum --;
+            }
+        } else if (i == minWeightNum) {
+            answer ++;
+            break;
         } else {
-            answer = answer + peopleArray.length;
+            break;
         }
-    }
-    
-    if (people.length > 1) {
-        count(people);
-    } else {
-        answer ++;
     }
     
     return answer;
